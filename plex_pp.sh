47ecd5cdca3600f5a63f9a345a8bcd7b82e3ef66
Ryan <thatvirtualboy@icloud.com>
2017-10-19 20:37:03 -0600

    Update plex_pp.sh

--- plex_pp.sh
+++ plex_pp.sh
@@ -9,8 +9,8 @@
 # FIRST, RUN COMCUT TO REMOVE COMMERCIALS, THEN TRANSCODE AND COMPRESS
 
 lockFile='/tmp/dvrProcessing.lock'
-sourceFile="$1"
-tempEncode="$1.mp4"
+inFile="$1"
+tmpFile="$1.mp4"
 dvrPostLog='/tmp/dvrProcessing.log'
 time=`date '+%Y-%m-%d %H:%M:%S'`
 handbrake=/PATH/TO/YOUR/INSTALL (mine is /usr/bin/HandBrakeCLI)
@@ -21,28 +21,28 @@ echo "'$time' Plex DVR Postprocessing script started" | tee $dvrPostLog
 # Check if post processing is already running
 while [ -f $lockFile ]
 do
-    echo "'$time' $lockFile' exists, sleeping processing of '$sourceFile'" | tee -a $dvrPostLog
+    echo "'$time' $lockFile' exists, sleeping processing of '$inFile'" | tee -a $dvrPostLog
     sleep 10
 done
 
 # Create lock file to prevent other post-processing from running simultaneously
-echo "'$time' Creating lock file for processing '$sourceFile'" | tee -a $dvrPostLog
+echo "'$time' Creating lock file for processing '$inFile'" | tee -a $dvrPostLog
 touch $lockFile
 
 # Run comcut
-echo "'$time' Comcut started on '$sourceFile'" | tee -a $dvrPostLog
-$cut "$sourceFile"
+echo "'$time' Comcut started on '$inFile'" | tee -a $dvrPostLog
+$cut "$inFile"
 
 # Encode file to MP4 with handbrake-cli
-echo "'$time' Transcoding started on '$sourceFile'" | tee -a $dvrPostLog
-$handbrake -i "$sourceFile" -o "$tempEncode" --preset="Apple 1080p30 Surround" --encoder-preset="veryfast"
+echo "'$time' Transcoding started on '$inFile'" | tee -a $dvrPostLog
+$handbrake -i "$inFile" -o "$tmpFile" --preset="Apple 1080p30 Surround" --encoder-preset="veryfast"
 
 # Overwrite original ts file with the transcoded file
 echo "'$time' File rename started" | tee -a $dvrPostLog
-mv -f "$tempEncode" "$sourceFile"
+mv -f "$tmpFile" "$inFile"
 
 #Remove lock file
-echo "'$time' Done processing '$sourceFile' Removing lock" | tee -a $dvrPostLog
+echo "'$time' Done processing '$inFile' Removing lock" | tee -a $dvrPostLog
 rm $lockFile
 
 exit 0
